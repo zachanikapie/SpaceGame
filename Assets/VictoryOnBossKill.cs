1bd3b49ab3abeb66007e48e320070a918895fa3f
Zach <G00304168@stu.raritanval.edu>
2024-04-30 02:11:46 -0400

    idk

--- Assets/VictoryOnBossKill.cs
+++ Assets/VictoryOnBossKill.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ObjectDestroyer : MonoBehaviour
 {
     public GameObject victoryCanvas;
+    public GameObject BossShip;
 
     private void OnDestroy()
     {
@@ -11,6 +12,18 @@ public class ObjectDestroyer : MonoBehaviour
         {
             // Enable the canvas when the object is destroyed
             victoryCanvas.SetActive(true);
+        }
+
+        // Loop through all GameObjects in the scene
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            // Destroy the object if tagged as "Enemy"
+            Destroy(obj);
+        }
+
+        if (BossShip != null)
+        {
+            BossShip.SetActive(false);
         }
     }
 
